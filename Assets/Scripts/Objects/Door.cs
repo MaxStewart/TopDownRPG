d1fb62c4b69ff78d5bf8eb9b66253c0818777eeb
Max Stewart <39015672+MaxStewart@users.noreply.github.com>
2020-04-01 15:42:46 +1300

    Added method to open door

---
+++ Assets/Scripts/Objects/Door.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DoorType
+{
+    key,
+    enemyCleared,
+    button
+}
+
+public class Door : Interactable
+{
+    [Header("Door variables")]
+    public DoorType thisDoorType;
+    public bool isOpen;
+    public GameObject door;
+    public BoxCollider2D triggerArea;
+
+    public Inventory playerInventory;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (m_PlayerInRange && thisDoorType == DoorType.key)
+            {
+                // Does player have key
+                if(playerInventory.numberOfKeys > 0)
+                {
+                    OpenDoor();
+                }
+            }
+        }
+    }
+
+    public void OpenDoor()
+    {
+        isOpen = true;
+        door.SetActive(false);
+        triggerArea.enabled = false;
+    }
+
+    public void CloseDoor()
+    {
+
+    }
+}
