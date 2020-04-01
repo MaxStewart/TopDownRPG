b07cdb7b49ba46d069e52e01d704bd18feaf8512
Max Stewart <39015672+MaxStewart@users.noreply.github.com>
2020-04-01 17:29:58 +1300

    Added heart containers

---
+++ Assets/Scripts/Objects/Heart.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : Powerup
+{
+    public FloatValue playerHealth;
+    public FloatValue heartContainers;
+    public float amountToIncrease;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player") && !other.isTrigger)
+        {
+            playerHealth.RuntimeValue += amountToIncrease;
+            if (playerHealth.m_InitialValue > heartContainers.RuntimeValue * 2f)
+            {
+                playerHealth.m_InitialValue = heartContainers.RuntimeValue * 2f;
+            }            
+            powerupSignal.Raise();
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Objects/Powerup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+    public Signal powerupSignal;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
