27c725496843c863cb55d484e9c9b977ef435289
Max Stewart <39015672+MaxStewart@users.noreply.github.com>
2020-04-14 15:09:58 +1200

    Updated Unity version and added coin
    New version 2018.4.20f1

---
+++ Assets/Scripts/Objects/Coin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : Powerup
+{
+
+    public Inventory inventory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        powerupSignal.Raise();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            inventory.coins += 1;
+            powerupSignal.Raise();
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/ScriptibleObjects/Inventory.cs
+++ Assets/Scripts/ScriptibleObjects/Inventory.cs
@@ -8,6 +8,7 @@ public class Inventory : ScriptableObject
     public Item currentItem;
     public List<Item> items = new List<Item>();
     public int numberOfKeys;
+    public int coins;
 
     public void AddItem(Item item)
     {
